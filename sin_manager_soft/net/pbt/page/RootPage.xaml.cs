bba33d5b596e88417516af32144bf3f8b1fb61e1
Karasev Sergey <karasevsergey479@gmail.com>
2021-04-12 14:01:13 +0300

    Удаление комментариев по умолчанию, реализация схемы пакетов

--- sin_manager_soft/net/pbt/page/RootPage.xaml.cs
+++ sin_manager_soft/net/pbt/page/RootPage.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
-namespace sin_manager_soft
+namespace sin_manager_soft.net.pbt.page
 {
     /// <summary>
     /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class RootPage : Page
     {
-        public MainPage()
+        public RootPage()
         {
             this.InitializeComponent();
         }
